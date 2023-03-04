7f3cc7116af08cd6959f6b057c25f3c37f30db99
SergioFloresGarcia <1430727@senati.pe>
2023-03-04 04:40:19 +0100

    Exercise1

---
+++ Exercise1.py
@@ -0,0 +1,9 @@
+#Desarrollen un algoritmo para hallar el area de un circulo, el usuario debe ingresar el radio.
+import math
+#Ingresar el radio
+print("coloque el radio")
+#Capturar el radio 
+radio=float(input())
+#Obtener el valor de pi
+pi=math.pi
+print("valor de PI",pi)
