c0399dc3e04cd2ae5e5e144e885a15123f30852d
SergioFloresGarcia <126987543+SergioFloresGarcia@users.noreply.github.com>
2023-03-04 06:18:53 +0100

    Update Exercise1.py

---
+++ Exercise1.py
@@ -0,0 +1,12 @@
+<<<<<<< HEAD
+=======
+#Desarrollen un algoritmo para hallar el area de un circulo, el usuario debe ingresar el radio.
+import math
+#Ingresar el radio
+print("coloque el radio")
+#Capturar el radio 
+radio=float(input())
+#Obtener el valor de pi
+pi=math.pi
+print("valor de PI",pi)
+>>>>>>> parent of 4fe02a2 (t)
