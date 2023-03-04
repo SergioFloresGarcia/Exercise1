867abc6be9ecf1b04c559b28deb4467bfdf13936
SergioFloresGarcia <126987543+SergioFloresGarcia@users.noreply.github.com>
2023-03-04 06:35:23 +0100

    Exercise2

---
+++ Exercise2.py
@@ -0,0 +1,5 @@
+#Desarrollar un algoritmo donde permite el ingreso de datos
+prime_numbers =[10, 15, 27, 100, 200]
+print("list:", prime_numbers)
+even_numbers =[10, 15, 27, 100, 200]
+print("list2;", even_numbers)
